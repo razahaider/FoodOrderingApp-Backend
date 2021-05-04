34accc059d09a9c6a57dcbfa8cd07f4e104666ae
Haider Raza <sayedhaiderr@gmail.com>
2021-05-05 02:03:30 +0530

    aPI-2 commit

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/ItemDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/ItemDao.java
@@ -1,5 +1,7 @@
 package com.upgrad.FoodOrderingApp.service.dao;
 
+import com.upgrad.FoodOrderingApp.service.entity.ItemEntity;
+import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -11,4 +13,59 @@ import java.util.List;
 @Repository
 public class ItemDao {
 
+  @PersistenceContext private EntityManager entityManager;
+
+  /**
+   *
+   *
+   * @param restaurant
+   * @return
+   */
+  public List<ItemEntity> getOrdersByRestaurant(RestaurantEntity restaurant) {
+    List<ItemEntity> items =
+        entityManager
+            .createNamedQuery("topFivePopularItemsByRestaurant", ItemEntity.class)
+            .setParameter(0, restaurant.getId())
+            .getResultList();
+    if (items != null) {
+      return items;
+    }
+    return Collections.emptyList();
+  }
+
+  /**
+   *
+   *
+   * @param itemUUID
+   * @return ItemEntity if found in database else null
+   */
+  public ItemEntity getItemByUUID(String itemUUID) {
+    try {
+      ItemEntity item =
+          entityManager
+              .createNamedQuery("itemByUUID", ItemEntity.class)
+              .setParameter("itemUUID", itemUUID)
+              .getSingleResult();
+      return item;
+    } catch (NoResultException nre) {
+      return null;
+    }
+  }
+
+  /**
+   *
+   *
+   * @param restaurantUuid
+   * @return List of ItemEntity
+   */
+  public List<ItemEntity> getAllItemsInCategoryInRestaurant(
+      final String restaurantUuid, final String categoryUuid) {
+    List<ItemEntity> items =
+        entityManager
+            .createNamedQuery("getAllItemsInCategoryInRestaurant", ItemEntity.class)
+            .setParameter("restaurantUuid", restaurantUuid)
+            .setParameter("categoryUuid", categoryUuid)
+            .getResultList();
+    return items;
+  }
 }
--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/RestaurantCategoryEntity.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/RestaurantCategoryEntity.java
@@ -13,4 +13,55 @@ import java.io.Serializable;
 @Table(name = "restaurant_category")
 public class RestaurantCategoryEntity implements Serializable {
 
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Column(name = "restaurant_id")
+  @NotNull
+  private Integer restaurantId;
+
+  @Column(name = "category_id")
+  @NotNull
+  private Integer categoryId;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public Integer getRestaurantId() {
+    return restaurantId;
+  }
+
+  public void setRestaurantId(Integer restaurantId) {
+    this.restaurantId = restaurantId;
+  }
+
+  public Integer getCategoryId() {
+    return categoryId;
+  }
+
+  public void setCategoryId(Integer categoryId) {
+    this.categoryId = categoryId;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    return new EqualsBuilder().append(this, obj).isEquals();
+  }
+
+  @Override
+  public int hashCode() {
+    return new HashCodeBuilder().append(this).hashCode();
+  }
+
+  @Override
+  public String toString() {
+    return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+  }
 }
